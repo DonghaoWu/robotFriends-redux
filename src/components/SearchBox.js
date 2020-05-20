551ee42b1dc34edc88b7b854d8bea7bb2cc88f14
Donghao Wu <donghao.wu.us@gmail.com>
2020-05-20 18:00:31 -0400

    working on service worker

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,12 +5,13 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
       <input
-        aria-label='Search Robots'
+        id="Search-Robots"
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
         onChange={searchChange}
       />
+      <label for="Search-Robots">Search here!</label>
     </div>
   );
 }
