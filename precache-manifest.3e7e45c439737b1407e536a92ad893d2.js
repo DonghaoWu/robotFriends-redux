2a92e6f47f971c0e794d8aeb68c6854d2eff18e3
Donghao Wu <donghao.wu.us@gmail.com>
2020-05-20 17:01:56 -0400

    Updates

---
+++ precache-manifest.3e7e45c439737b1407e536a92ad893d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a74534282e11881e51c661a51565515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f66c13a1e6262b42ca19",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "48d0b248f30832c460b3",
+    "url": "/static/css/main.93bae99b.chunk.css"
+  },
+  {
+    "revision": "f66c13a1e6262b42ca19",
+    "url": "/static/js/2.ba6adf4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ba6adf4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d0b248f30832c460b3",
+    "url": "/static/js/main.544ba2c2.chunk.js"
+  },
+  {
+    "revision": "62bbc721825e368031bd",
+    "url": "/static/js/runtime-main.72a576d5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
