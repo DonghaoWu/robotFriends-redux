6ab0f55d304b2b147e6dbc4cedc5c67297f02568
Donghao Wu <donghao.wu.us@gmail.com>
2020-08-30 16:54:11 -0400

    Updates

---
+++ precache-manifest.bbc142d34e3f635f26336651bd19a8b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174f6f7e811a379eb3e8fc17ec9e4290",
+    "url": "/PWA-robotFriends-redux-prod/index.html"
+  },
+  {
+    "revision": "223d54539413b906aee1",
+    "url": "/PWA-robotFriends-redux-prod/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1d0a92d2ed6c6d3cf46c",
+    "url": "/PWA-robotFriends-redux-prod/static/css/main.62828f13.chunk.css"
+  },
+  {
+    "revision": "223d54539413b906aee1",
+    "url": "/PWA-robotFriends-redux-prod/static/js/2.65ebfed1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PWA-robotFriends-redux-prod/static/js/2.65ebfed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0a92d2ed6c6d3cf46c",
+    "url": "/PWA-robotFriends-redux-prod/static/js/main.096c1c95.chunk.js"
+  },
+  {
+    "revision": "cfaca150cde7b50d1146",
+    "url": "/PWA-robotFriends-redux-prod/static/js/runtime-main.d03a4016.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/PWA-robotFriends-redux-prod/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DonghaoWu/PWA-robotFriends-redux-prod/precache-manifest.bca3364d2bf55b22a6a525b001505e12.js"
+  "/PWA-robotFriends-redux-prod/precache-manifest.bbc142d34e3f635f26336651bd19a8b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DonghaoWu/PWA-robotFriends-redux-prod/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PWA-robotFriends-redux-prod/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
